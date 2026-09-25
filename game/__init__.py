c3d42cb063fef65baed673e1f1cb9507cdeded76
Expedito Hebert <shinigamecontato@gmail.com>
2026-09-25 07:39:57 -0300

    deploy: build web do Simple World

--- game/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Modelo de representação do mundo de um jogo simples (IA para Jogos).
-
-Pacote com as estruturas que representam o mundo, suas entidades e uma
-simulação headless para análise de balanceamento.
-"""
-
-__version__ = "1.0.0"
