3154db842d87b999e0312a38073b7962cd3ca143
Expedito Hebert <shinigamecontato@gmail.com>
2026-09-25 08:21:00 -0300

    deploy: publica o jogo como site estatico com Pyodide
    Troca o build do pygbag (que travava no loading) por uma pagina que roda
    CPython em WebAssembly e desenha o mundo em Canvas 2D. Os artefatos do
    pygbag saem do branch.

---
+++ game/__init__.py
@@ -0,0 +1,7 @@
+"""Modelo de representação do mundo de um jogo simples (IA para Jogos).
+
+Pacote com as estruturas que representam o mundo, suas entidades e uma
+simulação headless para análise de balanceamento.
+"""
+
+__version__ = "1.0.0"
